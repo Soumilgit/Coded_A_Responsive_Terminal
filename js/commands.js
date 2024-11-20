a50086dabc97ce6212b445967b0af35345437951
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-11-20 19:35:43 +0530

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -4,7 +4,7 @@ var linkedin = "https://www.linkedin.com/in/soumilm30/";
 var instagram = "https://www.instagram.com/soumil_m.exe/";
 var github = "https://github.com/Soumilgit";
 var email = 'mailto:msoumil69@gmail.com';
-var resume = "https://drive.google.com/file/d/1k_xEQScYYTo4mLEQOEnAQOgoLDvSASIS/view?usp=sharing";
+var resume = "https://drive.google.com/file/d/15mcqfkvfp2yvTR5TYj1ArMs2CkruTrsU/view?usp=sharing";
 var portfolio = "https://soumilm.vercel.app/";
 whois = [
   "<br>",
